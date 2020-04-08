bbb394fc52247a9c84c66fdfdd6cd10820934882
Symion Edwards <symion.edwards@gmail.com>
2020-04-08 10:13:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ function theBeatlesPlay (musicians, instruments) {
   return array;
 }
 
-function johnLennonFacts(array) {
-  var i = 0;
- while (i < array.length) {
-       array[i] = array[i] + "!!!";
-    i++;  }
-   return array;
- }
-}
+function iLoveTheBeatles (number){
+  var array = [];
+  do {
+      array.push("I love the Beatles!");
+      number++;
+    }
+    while (number < 15);
+    return array;
 }

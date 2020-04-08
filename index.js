0c6c20f711b630866d9cfece1d55d03ea6b864fd
Symion Edwards <symion.edwards@gmail.com>
2020-04-08 09:54:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 function theBeatlesPlay (musicians, instruments) {
   var array = [];
-  for(var i=0; i < musicians.length; i++){
-    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  for (let i = 0, i <musicians.length, i++){
+    array.push(`${musicians[i]} plays ${instruments}`)
   }
-  return array;
-}
-
-function johnLennonFacts (array) {
-  var i = 0;
-  while (i < array.length) {
-    array[i] = array[i] +"!!!";
-    i++;
-  }
-  return array;
-}
-
-function iLoveTheBeatles(number) {
-  var array = [];
-  do {array.push("I love the Beatles!");
-  number++;}
-  while (number <15);
   return array;
 }
